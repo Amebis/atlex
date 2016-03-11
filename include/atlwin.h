e511f75430eaafb4bfb1c8a1495a3c116f48d3df
Simon Rozman <simon@rozman.si>
2016-03-11 09:30:58 +0100

    libatl renamed to atlex

--- include/atlwin.h
+++ include/atlwin.h
@@ -1,7 +1,7 @@
 /*
     Copyright 1991-2016 Amebis
 
-    This file is part of libatl.
+    This file is part of atlex.
 
     Setup is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
